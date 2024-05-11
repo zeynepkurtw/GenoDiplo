591bb72e770f09ef7ddbe1d0b042981f96bdf929
zeynepkurtw <zeynepkurt.w@gmail.com>
2024-05-11 14:13:09 +0200

    reorganized

--- scripts/Genomics/1_HybridGenomeAssemblyWorkflow/1_ReadsPreprocessing/TrimIlluminaReads.py
+++ scripts/Genomics/1_HybridGenomeAssemblyWorkflow/1_ReadsPreprocessing/TrimIlluminaReads.py
@@ -1,12 +1,17 @@
 from snakemake.shell import shell
 
-r1 = snakemake.input
-r2 = snakemake.input
-r1_u = snakemake.output #unique reads
-r2_u = snakemake.output
-r1_d = snakemake.output #duplicate reads
-r2_d = snakemake.output
-threads = snakemake.params.threads
+# Assuming the rule that calls this script is correctly defined as mentioned in previous communications:
+r1 = snakemake.input.r1  # Input forward reads
+r2 = snakemake.input.r2  # Input reverse reads
+r1_u = snakemake.output.r1_u  # Output forward paired
+r1_d = snakemake.output.r1_d  # Output forward unpaired
+r2_u = snakemake.output.r2_u  # Output reverse paired
+r2_d = snakemake.output.r2_d  # Output reverse unpaired
+threads = snakemake.params.threads  # Number of threads
 
-shell(f"""trimmomatic PE -threads {threads} {r1} {r2} {r1_u} {r1_d} {r2_u} {r2_d} CROP:100 MINLEN:50""")
-
+# Construct the Trimmomatic command correctly
+shell(
+    f"""trimmomatic PE -threads {threads} {r1} {r2} \
+    {r1_u} {r1_d} {r2_u} {r2_d} \
+    CROP:100 MINLEN:50"""
+)
