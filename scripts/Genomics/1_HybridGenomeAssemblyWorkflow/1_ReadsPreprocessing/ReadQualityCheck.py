f3674e94a0a5c7e646fa923e76598f7bc060b89b
zeynepkurtw <zeynepkurt.w@gmail.com>
2024-05-13 14:16:11 +0200

    reorganized

--- scripts/Genomics/1_HybridGenomeAssemblyWorkflow/1_ReadsPreprocessing/ReadQualityCheck.py
+++ scripts/Genomics/1_HybridGenomeAssemblyWorkflow/1_ReadsPreprocessing/ReadQualityCheck.py
@@ -6,4 +6,4 @@ out_dir = snakemake.output.out_dir
 #shell(f"cd {input_dir}")
 #shell(f"fastqc --threads 32 .")
 
-shell(f"fastqc --threads 32 {input_dir}/*.fastq.gz -o {out_dir}")
+shell(f"fastqc --threads 32 {input_dir}/*.fastq.gz -o {out_dir}/fastqc_before_trimming/")
