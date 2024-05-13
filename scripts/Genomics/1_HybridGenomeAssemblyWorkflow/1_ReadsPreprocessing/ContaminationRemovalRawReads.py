be66cfe4d245bc9de3fa6c0bf7827e06720a3b3a
zeynepkurtw <zeynepkurt.w@gmail.com>
2024-05-13 14:44:21 +0200

    reorganized

--- scripts/Genomics/1_HybridGenomeAssemblyWorkflow/1_ReadsPreprocessing/ContaminationRemovalRawReads.py
+++ scripts/Genomics/1_HybridGenomeAssemblyWorkflow/1_ReadsPreprocessing/ContaminationRemovalRawReads.py
@@ -13,8 +13,8 @@ if paired:
     clean_ill_R2 = snakemake.output.clean_ill_R2
 
     shell(f"bowtie2 -p {threads} -x {index} -1 {ill_R1} -2 {ill_R2} \
-          --un-conc {clean_ill_R1},{clean_ill_R2} \
-          --no-head --no-sq | samtools view -bS - > {contaminated_short}.bam")
+    --un-conc {clean_ill_R1},{clean_ill_R2} \
+    --no-head --no-sq | samtools view -bS - > {contaminated_short}.bam")
 
     shell(f"samtools sort -@ {threads} {contaminated_short}.bam -o {contaminated_short}_sorted.bam")
     shell(f"samtools index {contaminated_short}_sorted.bam")
@@ -30,8 +30,8 @@ else:
     clean_long = snakemake.output.clean_long
     # Align Nanopore reads and save unmapped reads
     shell(f"bowtie2 -p {threads} -x {index} -U {long_reads} \
-          --un {clean_long} \
-          --no-head --no-sq | samtools view -bS - > {contaminated_long}.bam")
+    --un {clean_long} \
+    --no-head --no-sq | samtools view -bS - > {contaminated_long}.bam")
 
     shell(f"samtools sort -@ {threads} {contaminated_long}.bam -o {contaminated_long}_sorted.bam")
     shell(f"samtools index {contaminated_long}_sorted.bam")
