74a8dfdbfe3717626d4844d376d7dee1dc04fefb
zeynepkurtw <zeynepkurt.w@gmail.com>
2024-05-13 10:36:58 +0200

    reorganized

--- scripts/Genomics/1_HybridGenomeAssemblyWorkflow/3_AssemblyEvaluation/MapShortReadsToAssembly.py
+++ scripts/Genomics/1_HybridGenomeAssemblyWorkflow/3_AssemblyEvaluation/MapShortReadsToAssembly.py
@@ -1,7 +1,7 @@
 from snakemake.shell import shell
 
 
-genome = snakemake.input.genome
+index = snakemake.input.index
 ill_R1 = snakemake.input.ill_R1
 ill_R2 = snakemake.input.ill_R2
 threads = snakemake.params.threads
@@ -10,6 +10,6 @@ bam = snakemake.output.bam
 bai = snakemake.output.bai
 
 
-shell(f"""bowtie2 -p {threads} -1 {ill_R1} -2 {ill_R2} -x {genome} | samtools sort -o {bam}""")
+shell(f"""bowtie2 -p {threads} -1 {ill_R1} -2 {ill_R2} -x {index} | samtools sort -o {bam}""")
 shell(f"""samtools index {bam} {bai}""")
 
