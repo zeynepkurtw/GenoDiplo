1ce654de900664688891dd0ee3abbcfcc59dfc99
zeynepkurtw <zeynepkurt.w@gmail.com>
2024-05-15 19:13:17 +0200

    reorganized

--- scripts/Genomics/1_HybridGenomeAssemblyWorkflow/2_Assemblers/MasurcaAssembler.py
+++ scripts/Genomics/1_HybridGenomeAssemblyWorkflow/2_Assemblers/MasurcaAssembler.py
@@ -4,7 +4,10 @@ config = snakemake.input.config
 path = snakemake.params.path
 out_dir = snakemake.output.out_dir
 
-#MaSuRCA (FLYE_ASSEMBLY = 1, SOAP_ASSEMBLY=0)
-shell(f"masurca {config} --path {path}")
-#shell(f"masurca {config}")
+shell(f"mkdir -p {out_dir}")
+shell(f"cp {config} {out_dir}")
+shell(f"cd {out_dir}")
+shell(f"masurca {config}")
 shell(f"bash assemble.sh > {out_dir}")
+
+
