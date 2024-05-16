a82cf94b3ea17671378876e18c789cfcadf0ad82
zeynepkurtw <zeynepkurt.w@gmail.com>
2024-05-16 09:33:57 +0200

    reorganized

--- scripts/Genomics/1_HybridGenomeAssemblyWorkflow/2_Assemblers/Polishing.py
+++ scripts/Genomics/1_HybridGenomeAssemblyWorkflow/2_Assemblers/Polishing.py
@@ -16,7 +16,7 @@ threads = snakemake.params.threads
 polished_assembly = snakemake.output.polished_assembly
 
 # Run Pilon
-shell(f"""java -Xmx16G -jar pilon.jar \
+shell(f"""pilon \
 --genome {assembly} \
 --frags {illumina_run1_R1},{illumina_run1_R2} \
 --frags {illumina_run2_R1},{illumina_run2_R2} \
