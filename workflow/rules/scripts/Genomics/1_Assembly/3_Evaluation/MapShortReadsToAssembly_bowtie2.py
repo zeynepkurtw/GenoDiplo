46908294d8cfecf751da751c652df73c28f3ff10
zeynepkurtw <zeynepkurt.w@gmail.com>
2024-05-19 12:43:49 +0200

    reorganized

--- workflow/rules/scripts/Genomics/1_Assembly/3_Evaluation/MapShortReadsToAssembly_bowtie2.py
+++ workflow/rules/scripts/Genomics/1_Assembly/3_Evaluation/MapShortReadsToAssembly_bowtie2.py
@@ -8,19 +8,16 @@ paired = snakemake.params.get('paired', False)
 
 sorted_bam = snakemake.output.sorted_bam
 
-#shell(f"""bowtie2 -p {threads} -1 {ill_R1} -2 {ill_R2} -x {index} | samtools sort -o {bam}""")
-#shell(f"""samtools index {bam} {bai}""")
-
 
 if paired:
     ill_R1 = snakemake.input.ill_R1
     ill_R2 = snakemake.input.ill_R2
-    shell(f"""bowtie2 -p {threads} -1 {ill_R1} -2 {ill_R2} -x {index} 2>bowtie2_paired.log | \
+    shell(f"""bowtie2 -p {threads} -1 {ill_R1} -2 {ill_R2} -x {index} | \
         samtools sort -o {sorted_bam} -""")
     shell(f"""samtools index {sorted_bam}""")
 
 else:
     single = snakemake.input.single
-    shell(f"""bowtie2 -p {threads} {single} -x {index} 2>bowtie2_single.log | \
+    shell(f"""bowtie2 -p {threads} {single} -x {index} | \
         samtools sort -o {sorted_bam} -""")
     shell(f"""samtools index {sorted_bam}""")
