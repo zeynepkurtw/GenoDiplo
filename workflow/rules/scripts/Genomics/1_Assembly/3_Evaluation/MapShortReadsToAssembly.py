c1a97ed9db845441d2b4e8048a56840c2e452b4d
zeynepkurtw <zeynepkurt.w@gmail.com>
2024-05-19 08:29:16 +0200

    reorganized

--- workflow/rules/scripts/Genomics/1_Assembly/3_Evaluation/MapShortReadsToAssembly.py
+++ workflow/rules/scripts/Genomics/1_Assembly/3_Evaluation/MapShortReadsToAssembly.py
@@ -5,7 +5,7 @@ threads = snakemake.params.threads
 paired = snakemake.params.get('paired', False)
 index = os.path.commonprefix(snakemake.input.index).rstrip(".")
 
-bam = snakemake.output
+bam = snakemake.output.bam
 
 if paired:
     ill_R1 = snakemake.input.ill_R1
