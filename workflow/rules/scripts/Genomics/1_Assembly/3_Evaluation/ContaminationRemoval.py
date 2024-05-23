cf795789d9d68cebdc8f24fc583e88f75aec03be
zeynepkurtw <zeynepkurt.w@gmail.com>
2024-05-23 15:19:32 +0200

    reorganized

--- workflow/rules/scripts/Genomics/1_Assembly/3_Evaluation/ContaminationRemoval.py
+++ workflow/rules/scripts/Genomics/1_Assembly/3_Evaluation/ContaminationRemoval.py
@@ -6,7 +6,10 @@ min_gc = snakemake.params.min_gc
 max_gc = snakemake.params.max_gc
 
 
-shell(f"""seqkit fx2tab -g {assembly} \
-| awk '$NF >= {min_gc} && $NF <= {max_gc}' \
-| cut -f 1 \
-| seqkit grep -f - {assembly} > {assembly_gc_filtered}""")
+shell(f"seqkit fx2tab -g {assembly} | cut -f 1,3 > gc_content_file.txt")
+
+shell(f"""
+    awk '$NF >= {min_gc} && $NF <= {max_gc}' gc_content_file.txt \
+    | cut -f 1 \
+    | seqkit grep -f - {assembly} > {assembly_gc_filtered}
+    """)
