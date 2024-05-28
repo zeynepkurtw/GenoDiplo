3b411a7aa049565f7eb65711352df604b61abb6d
zeynepkurtw <zeynepkurt.w@gmail.com>
2024-05-28 17:26:20 +0200

    reorganized

--- workflow/rules/scripts/Genomics/2_Annotation/1_Structural/GlimmerHMMAnnotation.py
+++ workflow/rules/scripts/Genomics/2_Annotation/1_Structural/GlimmerHMMAnnotation.py
@@ -3,9 +3,9 @@ from snakemake.shell import shell
 training_genes = snakemake.input.training_genes
 genome = snakemake.input.genome
 
-annotation = snakemake.output.annotation
+gff = snakemake.output.gff
 trained_genes = snakemake.output.trained_genes
 
 shell(f"trainGlimmerHMM {training_genes} -n 150 -v 50 -d {trained_genes}")
 #shell(f"python glimmerhmm.py")
-shell(f"glimmerhmm {genome} {training_genes} -o {annotation} -g")
+shell(f"glimmerhmm {genome} {training_genes} -o {gff} -g")
