696855cf4f3c5cc39b6fa3cb526a76c642306b33
zeynepkurtw <zeynepkurt.w@gmail.com>
2024-05-28 16:48:34 +0200

    reorganized

--- workflow/rules/scripts/Genomics/2_Annotation/1_Structural/ProdigalAnnotation.py
+++ workflow/rules/scripts/Genomics/2_Annotation/1_Structural/ProdigalAnnotation.py
@@ -2,7 +2,7 @@ from snakemake.shell import shell
 
 assembly = snakemake.input.assembly
 
-prodigal_gff = snakemake.output.prodigal_gff
-prodigal_aa = snakemake.output.prodigal_aa
+gff = snakemake.output.gff
+faa = snakemake.output.faa
  
-shell(f"prodigal -i {assembly} -o {prodigal_gff} -g 6 -f gff -c -a {prodigal_aa}""")
+shell(f"prodigal -i {assembly} -o {gff} -g 6 -f gff -c -a {faa}""")
